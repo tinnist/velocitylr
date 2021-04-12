8cb2560e82cfbd260c14f8a0b7a4e62260fcffa8
tinnist <82404492+tinnist@users.noreply.github.com>
2021-04-12 13:46:36 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,37 @@
-
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    agent.vy += -1
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    agent.setVelocity(0, 0)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    agent.setPosition(79, 55)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    agent.vx += -1
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    agent.vx += 1
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    agent.vy += 1
+})
+let agent: Sprite = null
+agent = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 2 2 . . . . . 
+    . . . . 2 . . . . . 2 2 . . . . 
+    . 2 . . . . 2 2 2 2 2 2 . . . . 
+    . . 2 2 . . 2 . . 2 2 2 . . . . 
+    . . . 2 2 2 2 2 2 . 2 2 . . . . 
+    . . . . . 2 2 2 . . 2 . . . . . 
+    . . . . . 2 2 . 2 2 . . . . . . 
+    . . . . . 2 . 2 . . . . . . . . 
+    . . . . . 2 . . 2 2 . . . . . . 
+    . . . . . 2 . . . 2 2 . . . . . 
+    . . . . . 2 . . . . . 2 . . . . 
+    . . . . . 2 2 2 2 2 2 2 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
